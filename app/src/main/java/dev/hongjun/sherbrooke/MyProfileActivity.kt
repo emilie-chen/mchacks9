0be3e77f0d25b205724956d6573a4388c1313463
Hongjun Chen <hongjun.chen2@mail.mcgill.ca>
2022-01-22 00:54:38 -0500

    Setup

---
+++ app/src/main/java/dev/hongjun/sherbrooke/MyProfileActivity.kt
@@ -0,0 +1,28 @@
+package dev.hongjun.sherbrooke
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import dev.hongjun.sherbrooke.ui.theme.ProjetSherbrookeTheme
+
+class MyProfileActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjetSherbrookeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dev/hongjun/sherbrooke/QRCodeConverter.kt
@@ -0,0 +1,5 @@
+package dev.hongjun.sherbrooke
+
+object QRCodeConverter {
+
+}
