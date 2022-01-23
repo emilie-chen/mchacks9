e16e48faa67639aa6d9ba456161956d255632bf0
Hongjun Chen <hongjun.chen2@mail.mcgill.ca>
2022-01-22 21:12:21 -0500

    Test card composable and card boilerplate code

--- app/src/main/java/dev/hongjun/sherbrooke/MainActivity.kt
+++ app/src/main/java/dev/hongjun/sherbrooke/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -18,8 +21,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -205,6 +214,57 @@ fun UserProfileEditor(navController: NavController) {
 // TODO : shows user info
 @Composable
 fun ProfileViewer(navController: NavController, userInfo: UserInfo) {
+    Column(
+        Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(10.dp)
+    ) {
+        repeat(10) {
+            TestCard()
+        }
+    }
+}
 
+@Composable
+fun TestCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clickable { },
+        elevation = 10.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(15.dp)
+        ) {
+            Text(
+                buildAnnotatedString {
+                    append("welcome to ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B8))
+                    ) {
+                        append("Jetpack Compose Playground")
+                    }
+                }
+            )
+            Text(
+                buildAnnotatedString {
+                    append("Now you are in the ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900)) {
+                        append("Card")
+                    }
+                    append(" section")
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfileViewerPreview() {
+    ProjetSherbrookeTheme {
+        ProfileViewer(navController = rememberNavController(), userInfo = dummyUserInfo)
+
+    }
 }
 
